38bb483801d5b5b04b7d0e857da9e65b2e8a8ad7
Jonas Ibrahim <jibrah14@student.aau.dk>
2017-09-20 11:38:20 +0200

    Began work on webhandler

--- BubbleBuster/BubbleBuster/Web/WebHandler.cs
+++ BubbleBuster/BubbleBuster/Web/WebHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,50 @@ namespace BubbleBuster.Web
 {
     public static class WebHandler
     {
-        
+        private static string _cred = "Bearer AAAAAAAAAAAAAAAAAAAAAPRw2QAAAAAAsXqGsVRPgYFVjSScMX3ZVa9YifA%3DkPvipEcLJj3QooYO7aVke3vZ9ruSJp9CgkTlKKtvlmSsGqLUdG";
+
+
+
+
+
+        public static string MakeRequest(string requestString)
+        {
+            return "";
+        }
+
+
+
+
+
+        public static T MakeRequest<T>(string requestString)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestString);
+            request.Headers[HttpRequestHeader.Authorization] = _cred;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+
+                
+            }
+        }
+    }
     }
 }
