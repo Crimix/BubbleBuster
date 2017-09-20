5946f2050284d7003a89dbeee59fcd6b5576986b
Crimix <christofferm@live.dk>
2017-09-20 11:34:48 +0200

    Added filehelper Added Json.net

--- BubbleBuster/BubbleBuster/Helper/FileHelper.cs
+++ BubbleBuster/BubbleBuster/Helper/FileHelper.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BubbleBuster.Helper
 {
-    class FileHelper
+    public static class FileHelper
     {
+
+        public static void WriteStringToFile(string folderName, string fileName, string data)
+        {
+            string folderPath = Path.GetTempPath() + folderName;
+            string filePath = Path.GetTempPath() + folderName + @"\" + fileName;
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            File.WriteAllText(filePath, data);
+        }
+
+        public static string ReadStringFromFile(string folderName, string fileName)
+        {
+            string folderPath = Path.GetTempPath() + folderName;
+            string filePath = Path.GetTempPath() + folderName + @"\" + fileName;
+            return File.ReadAllText(filePath);
+        }
+
+        public static void WriteObjectToFile(string folderName, string fileName, Object data)
+        {
+            string folderPath = Path.GetTempPath() + folderName;
+            string filePath = Path.GetTempPath() + folderName + @"\" + fileName;
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(data));
+        }
+
+        public static T ReadObjectFromFile<T>(string folderName, string fileName)
+        {
+            string folderPath = Path.GetTempPath() + folderName;
+            string filePath = Path.GetTempPath() + folderName + @"\" + fileName;
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(filePath));
+        }
     }
 }
