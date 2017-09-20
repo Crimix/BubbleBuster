df5675ee5a3df69512569225d77d81c151b1546e
Jonas Ibrahim <jibrah14@student.aau.dk>
2017-09-20 11:16:13 +0200

    Added classes

---
+++ BubbleBuster/BubbleBuster/JSON/FollowersID.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleBuster.JSON
+{
+    public class FollowersID
+    {
+        private List<int> followersIDs = new List<int>();
+
+        public FollowersID(List<int> id)
+        {
+            followersIDs = id;
+        }
+    }
+}
