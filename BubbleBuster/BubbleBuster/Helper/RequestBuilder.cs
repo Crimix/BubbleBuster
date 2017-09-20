ac784c233c8d4e218e20c8e9798ed7b73ba20966
Jonas Ibrahim <jibrah14@student.aau.dk>
2017-09-20 12:54:58 +0200

    We should be able to make requests now, but we appear to get an authorization error?

---
+++ BubbleBuster/BubbleBuster/Helper/RequestBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleBuster
+{
+    public enum DataType { friendsId, friendsObj }; //Expand based on what data is needed
+
+    public class RequestBuilder
+    {
+        string baseUrl = "https://api.twitter.com/1.1/";
+        
+        public string buildRequest(DataType returnType, string userName)
+        {
+            string returnString = baseUrl;
+
+            switch (returnType)
+            {
+                case DataType.friendsId:
+                    returnString += "friends/ids.json?";
+                    break;
+                case DataType.friendsObj:
+                    returnString += "friends/list.json?";
+                    break;
+                default:
+                    break;
+            }
+
+            returnString += ("screen name=" + userName);
+
+            return returnString;
+        }
+
+        public string buildRequest(DataType returnType, string userName, string cursor)
+        {
+            string returnString = buildRequest(returnType, userName);
+            returnString += "&cursor=" + cursor;
+            return returnString;
+        }
+    }
+}
--- BubbleBuster/BubbleBuster/Program.cs
+++ BubbleBuster/BubbleBuster/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using BubbleBuster;
 
 namespace BubbleBuster
 {
@@ -12,45 +13,19 @@ namespace BubbleBuster
     {
         static void Main(string[] args)
         {
-            string url = "https://api.twitter.com/1.1/friends/list.json?cursor=-1&screen_name=pewdiepie&skip_status=true&include_user_entities=false";
+            RequestBuilder rb = new RequestBuilder();
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            string returnedString = Web.WebHandler.MakeRequest(rb.buildRequest(DataType.friendsId, "pewdiepie"));
+            Console.WriteLine(returnedString);
 
-
-            string _cred = "Bearer AAAAAAAAAAAAAAAAAAAAAPRw2QAAAAAAsXqGsVRPgYFVjSScMX3ZVa9YifA%3DkPvipEcLJj3QooYO7aVke3vZ9ruSJp9CgkTlKKtvlmSsGqLUdG";
-            request.Headers[HttpRequestHeader.Authorization] = _cred;
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Stream receiveStream = response.GetResponseStream();
-                StreamReader readStream = null;
-
-                if (response.CharacterSet == null)
-                {
-                    readStream = new StreamReader(receiveStream);
-                }
-                else
-                {
-                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
-                }
-
-                string data = readStream.ReadToEnd();
-                SaveDataToFile(@"\BubbleBuster", "test.txt", data);
-
-                response.Close();
-                readStream.Close();
-
-                Console.WriteLine();
-                Console.ReadLine();
-            }
         }
+    
 
         private static void SaveDataToFile(string folderName, string fileName, string data)
         {
             string folderPath = Path.GetTempPath() + folderName;
             string filePath = Path.GetTempPath() + folderName + @"\" + fileName;
+
             if (!Directory.Exists(folderPath))
             {
                 Directory.CreateDirectory(folderPath);
