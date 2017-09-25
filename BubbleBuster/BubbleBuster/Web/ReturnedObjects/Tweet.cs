212307f40ba52d37c32c8cb643a39f6ee49be520
Crimix <christofferm@live.dk>
2017-09-25 14:50:59 +0200

    Added tweet

--- BubbleBuster/BubbleBuster/Web/ReturnedObjects/Tweet.cs
+++ BubbleBuster/BubbleBuster/Web/ReturnedObjects/Tweet.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace BubbleBuster.Web.ReturnedObjects
 {
     public class Tweet
     {
+        [JsonProperty("text")]
+        public string Text { get; set; }
 
     }
 }
