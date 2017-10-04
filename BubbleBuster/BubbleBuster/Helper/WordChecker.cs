da5118c90ad670a0c75e5dbd71b0e4fdf2594770
Jonas Ibrahim <jibrah14@student.aau.dk>
2017-10-04 11:30:33 +0200

    Started adding checks for news hyperlinks

--- BubbleBuster/BubbleBuster/Helper/WordChecker.cs
+++ BubbleBuster/BubbleBuster/Helper/WordChecker.cs
@@ -11,7 +11,8 @@ namespace BubbleBuster.Helper
     public class WordChecker
     {
         private static WordChecker _instance;
-        private List<string> ImportantWords = new List<string>();
+        private List<string> importantWords = new List<string>();
+        private Dictionary<string, int> newsHyperlinks = new Dictionary<string, int>();
 
         private WordChecker()
         {
@@ -26,27 +27,53 @@ namespace BubbleBuster.Helper
                 {
                     _instance = new WordChecker();
                     try
-                    {
-                        _instance.ImportantWords.AddRange(File.ReadAllLines("important_words"));
+                    {                        
+                        _instance.importantWords.AddRange(File.ReadAllLines("important_words"));
                     }
                     catch(Exception e)
                     {
                         Console.WriteLine("Important Words file not loaded: " + e.Message);
                     }
-                    
+
+                    try
+                    {
+                        foreach (string link in File.ReadAllLines("news_hyperlinks"))
+                        {
+                            string[] arr = link.Split(';');
+                            _instance.newsHyperlinks.Add(arr[0], Convert.ToInt32(arr[1]));
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("News Sources file not loaded: " + e.Message);
+                    }
+
                 }
                 return _instance;
             }
         }
 
-        public void checkTweetForWords(Tweet tweet)
+        public bool CheckTweetForWords(Tweet tweet)
         {
-            List<string> returnList = new List<string>();
-            foreach(string word in ImportantWords)
+            foreach(string word in importantWords)
             {
                 if (tweet.Text.Contains(word))
                 {
                     tweet.ImportantWords.Add(word);
+                }
+            }
+
+            return tweet.ImportantWords.Count > 0;
+        }
+
+        public void CheckTweetForHyperlink(Tweet tweet)
+        {
+            foreach (Url link in tweet.Entities.Urls)
+            {
+                if (newsHyperlinks.Keys.Contains(link.ExpandedUrl))
+                {
+                    
                 }
             }
         }
