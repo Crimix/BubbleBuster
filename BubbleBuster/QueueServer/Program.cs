078ec574f5da130fe5748f4ec06e7090f00b1245
Mathias Leding <mledin14@student.aau.dk>
2017-10-30 13:25:52 +0100

    QueueServer Beta 0.1

--- BubbleBuster/QueueServer/Program.cs
+++ BubbleBuster/QueueServer/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using QueueServer.Classes;
 
 namespace QueueServer
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TaskQueue();
         }
+        private static async void TaskQueue()
+        {
+            const int taskLimit = 5;
+            Queue<Task> taskQueue = new Queue<Task>();
+            List<Task> runningTasksList = new List<Task>();
+            while (true)
+            {
+                while (ThereIsNewTask())
+                {
+                    Task newTask = Task.Factory.StartNew(() =>
+                    {
+                        ServerTask st = new ServerTask();
+
+                    });
+                    taskQueue.Enqueue(newTask);
+                }
+
+                while (runningTasksList.Count < taskLimit)
+                {
+                    if (taskQueue.Count != 0)
+                    {
+                        runningTasksList.Add(taskQueue.Peek());
+                        await RunTaskAsync(taskQueue.Dequeue());
+                    }
+                }
+                
+                Thread.Sleep(420); //so it doesn't check all the time.
+                Task.WaitAll(runningTasksList.ToArray());
+            }           
+        }
+
+        private static bool ThereIsNewTask()
+        {
+            bool newTaskYes = false;
+            /* check for new tasks */
+            
+            return newTaskYes;
+        }
+
+        private static Task RunTaskAsync(Task task)
+        {
+            return Task.Run(() =>
+            {
+                RunTask(task);
+            });
+        }
+        private static void RunTask (Task task)
+        {
+            task.Start();
+        }
+
+        private void 
     }
 }
