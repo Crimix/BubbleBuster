c9c2cd9100b5cfb90cbfeef1318c37f23be8c4e7
Jonas Ibrahim <jibrah14@student.aau.dk>
2017-11-08 10:01:39 +0100

    Changed WordChecker to TweetAnalyzer, and finished the decoration of tweets with information from analysis.

---
+++ BubbleBuster/BubbleBuster/Helper/TweetAnalyzer.cs
@@ -0,0 +1,116 @@
+﻿using BubbleBuster.Web.ReturnedObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BubbleBuster.Helper.HelperObjects;
+
+namespace BubbleBuster.Helper
+{
+    public class TweetAnalyzer
+    {
+        private static TweetAnalyzer _instance;
+        private Dictionary<string, int> analysisWords = new Dictionary<string, int>();
+        private Dictionary<string, int> newsHyperlinks = new Dictionary<string, int>();
+        private Dictionary<string, HashtagObj> hashtags = new Dictionary<string, HashtagObj>();
+
+        private TweetAnalyzer()
+        {
+
+        }
+
+        public static TweetAnalyzer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TweetAnalyzer();
+                    _instance.analysisWords = FileHelper.GetAnalysisWords();
+                    _instance.newsHyperlinks = FileHelper.GetHyperlinks();
+                    _instance.hashtags = FileHelper.GetHashtags();
+                }
+                return _instance;
+            }
+        }
+
+        public List<Tweet> AnalyzeAndDecorateTweets(List<Tweet> tweetList)
+        {
+            List<Tweet> returnList = tweetList;
+
+            returnList = CalculateSentiment(returnList);
+            returnList = CalculateHashtagSentiment(returnList);
+            returnList = CalculateUrlSentiment(returnList);
+
+            return returnList;
+        }
+
+        //Calculates the general sentiment of a tweet. This is done by looking at the positive and negative words.
+        private List<Tweet> CalculateSentiment(List<Tweet> tweetList)
+        {
+            List<Tweet> returnList = tweetList;
+
+            foreach (Tweet tweet in returnList)
+            {
+                foreach (string word in analysisWords.Keys)
+                {
+                    if (tweet.Text.Contains(word))
+                    {
+                        if (analysisWords[word] == 1)
+                            tweet.positiveValue++;
+                        else
+                            tweet.negativeValue++;
+                    }
+                }
+            }
+            return returnList;
+
+        }
+
+        private List<Tweet> CalculateHashtagSentiment(List<Tweet> tweetList)
+        {
+            List<Tweet> returnList = tweetList;
+
+            foreach (Tweet tweet in returnList)
+            {
+                foreach (string hashtag in hashtags.Keys)
+                {
+                    if (tweet.Text.Contains(hashtag))
+                    {
+                        int sentiment = tweet.getSentiment();
+
+                        if (sentiment > 1)
+                            tweet.hashtagBias += hashtags[hashtag].pos;
+                        else if (sentiment == 1)
+                            tweet.hashtagBias += hashtags[hashtag].bas;
+                        else if (sentiment < 1)
+                            tweet.hashtagBias += hashtags[hashtag].neg;
+                    }
+                }
+            }
+
+            return returnList;
+        }
+
+        private List<Tweet> CalculateUrlSentiment(List<Tweet> tweetList)
+        {
+            List<Tweet> returnList = tweetList;
+
+            foreach (Tweet tweet in tweetList)
+            {
+                foreach (Url link in tweet.Entities.Urls)
+                {
+                    string shortenedUrl = UrlHelper.Instance.ShortenUrl(link.ExpandedUrl);
+                    if (newsHyperlinks.Keys.Contains(shortenedUrl))
+                    {
+                        tweet.mediaBias += newsHyperlinks[shortenedUrl];
+                    }
+                }
+            }
+
+            return returnList;
+        }
+    }
+}
