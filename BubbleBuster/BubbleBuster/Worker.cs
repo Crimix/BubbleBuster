887ca0ff66104ec812e472f1c11cf1d4227d068e
Crimix <christofferm@live.dk>
2017-11-24 12:00:35 +0100

    - Write the result of calling the post method to the db server to the log, such that we know if it was posted

--- BubbleBuster/BubbleBuster/Worker.cs
+++ BubbleBuster/BubbleBuster/Worker.cs
@@ -36,9 +36,9 @@ namespace BubbleBuster
             double filterPol = filterBubbleResults[0] + filterBubbleResults[1];
 
 
-            new WebHandler(apiKey).DBPostRequest(RequestBuilder.BuildRequest(DataType.database, apiKey, "name=" + user.Name, "twitterID=" + user.Id, "pol_var=" + userpol, "lib_var=" + 0, "fpol_var=" + filterPol, "flib_var=" + 0, "protect=" + Convert.ToInt32(user.IsProtected)));
+            bool post = new WebHandler(apiKey).DBPostRequest(RequestBuilder.BuildRequest(DataType.database, apiKey, "name=" + user.Name, "twitterID=" + user.Id, "pol_var=" + userpol, "lib_var=" + 0, "fpol_var=" + filterPol, "flib_var=" + 0, "protect=" + Convert.ToInt32(user.IsProtected)));
 
-            Log.Info("Done!!! " + filterBubble.Count);
+            Log.Info("Done!!! " + filterBubble.Count + " success " + post);
         }
     }
 }
