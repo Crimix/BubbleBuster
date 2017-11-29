1617e9b653eb6818c10ab9743633e5ef57b2f172
Jonathan Magnussen <jnma14@student.aau.dk>
2017-11-29 14:50:50 +0100

    Added Classifier and removed excess methods from TextProcessor
    TextProcessor has been cleaned, and is only currently used by the
    Classifier

---
+++ BubbleBuster/BubbleBuster/Helper/Classifier.cs
@@ -0,0 +1,54 @@
+﻿using Accord.IO;
+using Accord.MachineLearning.Bayes;
+using Accord.Statistics.Distributions.Univariate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BubbleBuster.Web.ReturnedObjects;
+using Accord.MachineLearning;
+using TextProcesserLib;
+
+namespace BubbleBuster.Helper
+{
+    class Classifier
+    {
+        BagOfWords bagOfWords;
+        TextProcessor tp;
+
+        public Classifier()
+        {
+            tp = new TextProcessor();
+        }
+
+        public void RunNaiveBayes(List<Tweet> tweets)
+        {
+            Serializer.Load(@"my_NB.accord", out NaiveBayes<NormalDistribution> nb);
+
+            double[][] inputs = FormatTweets(tweets);
+
+            int[] answers = nb.Decide(inputs);
+        }
+
+        public double[][] FormatTweets (List<Tweet> tweets)
+        {
+            List<string> _tweets = new List<string>();
+            foreach (Tweet item in tweets)
+            {
+                _tweets.Add(item.Text);
+            }
+            
+            //Load a trained Bag of Words
+            //bagOfWords = ...
+
+            //string[][] tokens = tweets.ToArray().Tokenize();
+            string[][] tokens = tp.Tokenizer(_tweets);
+            
+            double[][] input = bagOfWords.Transform(tokens);
+
+            return input;
+        }
+    }
+}
+
